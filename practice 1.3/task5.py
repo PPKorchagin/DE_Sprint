c44d2b2157b1f14e44a557f18f9a321fed4af988
PPKorchagin <ppkorchagin@stv.beeline.ru>
2022-10-25 00:38:34 +0300

    update

--- practice 1.3/task5.py
+++
@@ -1,8 +0,0 @@
-
-def multiplication(a, b):
-    firstnumber = str(a)
-    secondnumber = str(b)
-    Multiplication = int(firstnumber, 2) * int(secondnumber, 2)
-    return format(Multiplication, 'b')
-
-print(multiplication(111, 101))
